6599cbd9a0e3dc43f4c07ad07a50bc41e9c499d1
woni <320753691@qq.com>
2021-10-13 20:38:27 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Milkdown />
+</template>
+
+<script>
+import Milkdown from "./components/Milkdown.vue";
+export default {
+  name: "App",
+  components: {
+    Milkdown,
+  },
+};
+</script>
+<style>
+body {
+  background: rgba(var(--background), 1);
+}
+</style>
